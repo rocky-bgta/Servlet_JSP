5d9b01d161221666e6d5feb797699758910fb8bf
rocky-bgta <rocky.bgta@gmail.com>
2016-02-04 19:17:24 +0600

    Servlet Working

--- Java_Web_Certification/src/main/java/com/example/web/BeerSelect.java
+++ Java_Web_Certification/src/main/java/com/example/web/BeerSelect.java
@@ -1,5 +1,29 @@
 package com.example.web;
 
-public class BeerSelect {
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class BeerSelect extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub
+				PrintWriter out = resp.getWriter();
+				out.println("rocky");
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub
+				PrintWriter out = resp.getWriter();
+				out.println("rocky");
+	}
 
 }
