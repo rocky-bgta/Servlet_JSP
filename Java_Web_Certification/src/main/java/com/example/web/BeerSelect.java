5a707113a4bcd4da60d53fa5ffd2041bdfc76a4f
rocky-bgta <rocky.bgta@gmail.com>
2016-02-05 12:39:01 +0600

    Jsp

--- Java_Web_Certification/src/main/java/com/example/web/BeerSelect.java
+++ Java_Web_Certification/src/main/java/com/example/web/BeerSelect.java
@@ -2,28 +2,50 @@ package com.example.web;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Iterator;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.example.model.BeerExpert;
+
 public class BeerSelect extends HttpServlet {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		// TODO Auto-generated method stub
-				PrintWriter out = resp.getWriter();
-				out.println("rocky");
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		
 	}
 
 	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		// TODO Auto-generated method stub
-				PrintWriter out = resp.getWriter();
-				out.println("rocky");
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		//PrintWriter out = response.getWriter();
+		//out.println("Beer Selection Advice<br>");
+		String c = request.getParameter("color");
+		//out.println("<br>Got beer color " + c);
+		
+		BeerExpert be = new BeerExpert();
+		ArrayList<?> result =  be.getBrands(c);
+		
+		request.setAttribute("styles", result);
+		RequestDispatcher view = request.getRequestDispatcher("result.jsp");
+		view.forward(request, response);
+		
+	/*	Iterator<?> it = result.iterator();
+		while(it.hasNext()){
+			out.print("<br>try: " + it.next());
+		}*/
+		
 	}
 
 }
