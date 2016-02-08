6980e5140ba2b119a6a9a56f90fc9991d4414923
rocky-bgta <rocky.bgta@gmail.com>
2016-02-08 23:25:35 +0600

    Listaner

--- Java_Web_Certification/src/main/java/com/example/model/Dog.java
+++ Java_Web_Certification/src/main/java/com/example/model/Dog.java
@@ -1,9 +1,13 @@
 package com.example.model;
 
+import java.io.Serializable;
+
+import javax.servlet.http.HttpSessionActivationListener;
 import javax.servlet.http.HttpSessionBindingEvent;
 import javax.servlet.http.HttpSessionBindingListener;
+import javax.servlet.http.HttpSessionEvent;
 
-public class Dog implements HttpSessionBindingListener {
+public class Dog implements HttpSessionBindingListener, HttpSessionActivationListener,Serializable {
 	private String breed;
 	
 	public Dog(String breed){
@@ -26,6 +30,18 @@ public class Dog implements HttpSessionBindingListener {
 
 	@Override
 	public void valueUnbound(HttpSessionBindingEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void sessionWillPassivate(HttpSessionEvent se) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void sessionDidActivate(HttpSessionEvent se) {
 		// TODO Auto-generated method stub
 		
 	}
